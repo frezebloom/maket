332a6eea604212a27c95d3872d105178765c4d5d
Mikhail Yarema <juxx@bk.ru>
2018-10-28 22:13:35 +0300

    close / open basket

--- dev/js/minicart.js
+++ dev/js/minicart.js
@@ -1,9 +1,24 @@
 class Minicart {
+  //Открыть <---> Закрыть Панель корзины товаров
   open() {
+    let opened = false;
     const minicartPanel = document.querySelector(".minicart");
     const minicartButton = document.querySelector(".shopping_cart_button");
+    const container = document.getElementsByClassName("container");
     minicartButton.addEventListener("click", () => {
-      minicartPanel.style.marginRight = "0px";
+      if (!opened) {
+        minicartPanel.style.marginRight = "0px";
+        for (let i = 0; i < container.length; i++) {
+          container[i].style.marginRight = "450px";
+        }
+        opened = true;
+      } else {
+        minicartPanel.style.marginRight = "-350px";
+        for (let i = 0; i < container.length; i++) {
+          container[i].style.marginRight = "0px";
+        }
+        opened = false;
+      }
     });
   }
 }
